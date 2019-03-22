f35ff4b81668e497ae6e554534c9b7959e153cb8
Jonathan Rey A. Sarabosing <38988189+jreysarabosing@users.noreply.github.com>
2019-03-22 16:11:33 +0800

    Code V2.0

---
+++ Thesis/transmitter_2.0/transmitter_2.0.ino
@@ -0,0 +1,143 @@
+//Libraries being used
+#include <SPI.h>
+#include <Chrono.h>
+#include <RH_RF95.h>
+
+//Definitions
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+#define RF95_FREQ 915.0 //Must match RX's freq!
+#define LED 13
+
+//Instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+//Instance of stopwatch timer
+Chrono StopWatch(Chrono::SECONDS);
+
+//Global variables for Ultrasonic
+const int pwPin1 = 3;
+float sensor, mm1, mm, pw1, distance1;
+int t = 0;
+
+void setup(){
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  pinMode(pwPin1, INPUT);
+  delay(100);
+
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  while (!rf95.init()){
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(500);
+  }
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  while (!rf95.setFrequency(RF95_FREQ)){
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(500);
+  }
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void read_sensors(){
+  /*
+  Scale factor is (Vcc/512) per inch. A 5V supply yields ~9.8mV/in
+  Arduino analog pin goes from 0 to 1024, so the value has to be divided by 2 to get the actual inches
+  */
+  sensor = pulseIn(pwPin1, HIGH);
+  mm1 = sensor;
+  pw1=25.4/147;
+  mm=mm1*pw1;
+  distance1 = mm/25.4;
+}
+
+void loop(){
+  char radiopacket[20];
+  float spd;
+  read_sensors();
+
+  if(distance1>6 && distance1<30){
+    if(StopWatch.isRunning()==false){
+      StopWatch.restart();
+    }
+  }
+  else{
+      t=StopWatch.elapsed();
+      spd=12/t;
+      itoa(spd, radiopacket, 10);
+      radiopacket[19] = 0;
+      Serial.println(radiopacket);
+      rf95.send((uint8_t*)radiopacket, 20);
+      rf95.waitPacketSent();
+      Serial.println("Sent");
+      digitalWrite(LED, HIGH);
+      digitalWrite(LED, LOW);
+      t=0;
+      StopWatch.stop();
+    }
+    delay(50);
+
+    
+  /*if(distance1 <200 && distance1 > 6){
+    StopWatch.restart();
+    t = StopWatch.elapsed();
+    spd = 12/t; //inches per sec
+    itoa(spd, radiopacket, 10);
+    radiopacket[19] = 0;
+    Serial.println(radiopacket);
+    rf95.send((uint8_t*)radiopacket, 20);
+    rf95.waitPacketSent();
+    Serial.println("Sent");
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED, LOW);
+    t=0;
+    StopWatch.restart();
+  }else{
+    t = StopWatch.elapsed();
+    spd = 0;
+    itoa(spd, radiopacket, 10);
+    radiopacket[19] = 0;
+    Serial.println(radiopacket);
+    rf95.send((uint8_t*)radiopacket, 20);
+    rf95.waitPacketSent();
+    Serial.println("Sent");
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED, LOW);
+    t=0;
+    StopWatch.restart();
+  }*/
+}
