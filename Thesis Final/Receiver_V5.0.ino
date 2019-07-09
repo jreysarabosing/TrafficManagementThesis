58cb26be901926ebfa5f8d7fdd10a3e3c94fc692
Jonathan Rey A. Sarabosing <38988189+jreysarabosing@users.noreply.github.com>
2019-07-09 16:55:22 +0800

    Add files via upload

---
+++ Thesis Final/Receiver_V5.0.ino
@@ -0,0 +1,101 @@
+// Arduino9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Arduino9x_TX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+#define RF95_FREQ 915.0 //Must match RX's freq!
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("CLEARDATA");
+  Serial.println("LABEL,Date,Time,Speed A(kmph),Speed B(kmph)");
+  Serial.println("RESETTIMER");
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(500);
+  }
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  while (!rf95.setFrequency(RF95_FREQ)) {
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(500);
+  }
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+void loop(){
+  if (rf95.available()){
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len)){
+      if(buf[0]=='A'){
+        Serial.print("DATA,DATE,TIME,");
+        for(int i=1;i<len;i++){
+          buf[i-1]=buf[i];
+        }
+        Serial.println((char*)buf);
+      }else if(buf[0]=='B'){
+        Serial.print("DATA,DATE,TIME,,");
+        for(int i=1;i<len;i++){
+          buf[i-1]=buf[i];
+        }
+        Serial.println((char*)buf);
+      }else{
+        Serial.println((char*)buf);
+        //Serial.println("No data");
+      }
+    }
+  }
+}
